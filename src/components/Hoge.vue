3ee41e11e6ea6160a368f5b9a1ec3ee384977aa7
baado <748475+baado@users.noreply.github.com>
2023-02-24 22:37:27 +0900

    first commit

---
+++ src/components/Hoge.vue
@@ -0,0 +1,21 @@
+<script setup>
+// 変数
+const msg = 'Hello!'
+
+// 関数
+function log() {
+  console.log(msg)
+}
+
+// JavaScriptのenum、これは無いのでエラーする
+// enum ZeroOrOne {
+//   Zero = 0,
+//   One = 1,
+// }
+ 
+// console.log(ZeroOrOne[0]);
+</script>
+
+<template>
+  <button @click="log">{{ msg }}</button>
+</template>
